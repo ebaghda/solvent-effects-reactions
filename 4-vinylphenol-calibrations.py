6d5f68d1036e8a866a6408715e6daf01f58b38e8
Ezra Baghdady <ezra.baghdady@colorado.edu>
2024-12-15 15:53:06 -0700

    add 4-vinylphenol-calibrations.py

---
+++ 4-vinylphenol-calibrations.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from linear_fit import linear_fit
+# initialize figure preferences
+from set_fig_prefs import set_fig_prefs
+set_fig_prefs()
+#fig_dpi = 90 #set figure dpi
+
+# build 4-vinylphenol calibration data = VP Dataframe
+VP = pd.DataFrame()
+VP["concentration"] = np.array([0.03125, 0.3125, 3.125, 6.25, 12.5, 25, 50])
+VP["VP_area"] = np.array([0.41, 1.6, 27.2, 64.7, 136, 294, 590.8])
+VP["ISTD_area"] = np.array([394.5, 392.4, 394.4, 412.5, 382.6, 377.7, 366.9])
+VP["peak_ratio"] = VP["VP_area"]/VP["ISTD_area"]
+VP["RRF"] = VP["concentration"]*VP["ISTD_area"]/VP["VP_area"]/50
+
+## generate figures
+# plot RRF vs. concentration
+fig, ax = plt.subplots()
+plt.scatter(VP["concentration"], VP["RRF"], s=20, c="Black")
+plt.xlabel("4-Vinylphenol Concentration (mM)")
+plt.ylabel("Relative Response Factor (-)")
+
+# plot calibration curve
+from plot_calibration_curve import plot_calibration_curve
+plot_calibration_curve(VP["peak_ratio"], VP["concentration"], "Peak Ratio","4-Vinylphenol Concentration (mM)")
