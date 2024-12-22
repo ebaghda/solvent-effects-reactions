a62c594daa51d40c6a860cbba1219d8fa776148a
Ezra Baghdady <ezra.baghdady@colorado.edu>
2024-12-21 19:36:41 -0700

    add convert_csv_to_parquet.py

--- convert_csv_to_parquet.py
+++ convert_csv_to_parquet.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def to_parquet(filename):
     """
     Convert a csv to a parquet file.
@@ -7,3 +9,7 @@ def to_parquet(filename):
     print("reading csv... header: 0 | skiprows: [1,2] | encoding: latin")
     df.to_parquet(filename.replace(".csv", ".parquet"))
     print("Converted csv to parquet")
+
+to_parquet(r"vinylphenol transfer hydrogenation(data).csv")
+df = pd.read_parquet(r"vinylphenol transfer hydrogenation(data).parquet")
+print(df.head(20))
