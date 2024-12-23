6efd82f9623182e6c2ed365eb66767c945a4eb88
Ezra Baghdady <ezra.baghdady@colorado.edu>
2024-12-23 08:25:55 -0700

    updated ethylphenol_generation_vs_IPA_concentration.py
    increased font sizes to batter match OriginPro format (my format) and modified tick labels

--- ethylphenol_generation_vs_IPA_concentration.py
+++ ethylphenol_generation_vs_IPA_concentration.py
@@ -9,15 +9,18 @@ df = DF[(DF["catalyst"] == "Pd") & (DF["temperature_C"] == 75) & (DF["formate_mM
 
 plt.errorbar(df[df["IPA_molefrac"] >= 0.0]["IPA_molefrac"], df[df["IPA_molefrac"] >= 0.0]["EP_generation_rate"], yerr=df[df["IPA_molefrac"] >= 0.0]["EP_generation_rate_std_err"], c="k", capsize=4, linewidth=1, zorder=0)
 
-df[df["IPA_molefrac"] == 0.0].plot.scatter("IPA_molefrac", "EP_generation_rate", ax=ax, label="0% IPA",s=30, c="b")
-df[df["IPA_molefrac"] == 0.1].plot.scatter("IPA_molefrac", "EP_generation_rate", ax=ax, label="10% IPA",s=30, c="#58a8f9")
-df[df["IPA_molefrac"] == 0.2].plot.scatter("IPA_molefrac", "EP_generation_rate", ax=ax, label="20% IPA",s=30, c="g")
+df[df["IPA_molefrac"] == 0.0].plot.scatter("IPA_molefrac", "EP_generation_rate", ax=ax, label="0% IPA",s=60, c="b", edgecolor="k")
+df[df["IPA_molefrac"] == 0.1].plot.scatter("IPA_molefrac", "EP_generation_rate", ax=ax, label="10% IPA",s=60, c="#58a8f9", edgecolor="k")
+df[df["IPA_molefrac"] == 0.2].plot.scatter("IPA_molefrac", "EP_generation_rate", ax=ax, label="20% IPA",s=60, c="g",edgecolor="k")
 
-plt.xlabel("IPA Concentration (mole/mole)")
-plt.ylabel("Ethylphenol Generation Rate (mM/g s)")
+plt.xlabel("IPA Concentration (mole/mole)", labelpad=15, fontsize=24)
+plt.ylabel("Ethylphenol Generation Rate (mM/g s)", labelpad=15, fontsize=24)
+ax.set_xticklabels(["0", "0.1", "0.2"], fontsize=20)
+ax.set_yticklabels(["0", "0.2", "0.4", "0.6", "0.8", "1.0"], fontsize=20)
 ax.set_xticks([0, 0.1, 0.2])
-ax.set_ylim(-0.1, 1.1)
+ax.set_ylim(-0.05, 1)
 ax.set_yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0])
 ax.legend().set_visible(False) #hide the legend
+ax.spines["top"].set_visible(True)
 plt.savefig("ethylphenol_generation_vs_IPA_concentration_900dpi.png", bbox_inches="tight", dpi=900)
 print('figure saved to "ethylphenol_generation_vs_IPA_concentration_900dpi.png"')
